4867d64a3bbd9db08eaabd6901ecea10063374d0
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-01-13 17:58:26 -0500

    Added python files

---
+++ scapy.py
@@ -0,0 +1,9 @@
+def printMsg(msg):
+    foreground='color'
+    listOfForegorund=list(foreground)
+    print(msg+", foreground: "+foreground)
+oldMessage=8
+message='Message to the printer'
+oldMessageStr='Old message to your printer'
+def printOldMsg():
+    print(oldMessageStr)
