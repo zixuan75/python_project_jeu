1a48cbdf907c506d72907512398122db60f212e5
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-02-10 21:22:09 -0500

    Revert "Added python files"

--- scapy.py
+++
@@ -1,9 +0,0 @@
-def printMsg(msg):
-    foreground='color'
-    listOfForegorund=list(foreground)
-    print(msg+", foreground: "+foreground)
-oldMessage=8
-message='Message to the printer'
-oldMessageStr='Old message to your printer'
-def printOldMsg():
-    print(oldMessageStr)
